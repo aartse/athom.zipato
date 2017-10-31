b8ffaf8344b406cb7f7147f1b57ca5e930a722b9
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2017-10-31 23:31:28 +0100

    added 2 flow actions for siren

--- drivers/PH-PSE02-1B.EU/driver.js
+++ drivers/PH-PSE02-1B.EU/driver.js
@@ -6,7 +6,7 @@ const ZwaveDriver	= require('homey-zwavedriver');
 //https://products.z-wavealliance.org/products/2421
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
-	debug: true,
+	debug: false,
 	capabilities: {
         'alarm_tamper': [
             {
@@ -20,14 +20,14 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
                 'command_class'             : 'COMMAND_CLASS_SENSOR_BINARY',
                 'command_report'            : 'SENSOR_BINARY_REPORT',
                 'command_report_parser'     : function( report ){
-                    console.log(report);
 
                     //default tamper notification
                     if (report['Sensor Type'] == 'Tamper') {
                         return (report['Sensor Value'] == 'detected an event');
                     }
                     //stop tamper when siren state report sends a stop alarm report
-                    if (report['Sensor Type'] == 'General Purpose' && report['Sensor Value'] == 'Stop play') {
+                    if ((report['Sensor Type'] == 'General Purpose' && report['Sensor Value'] == 'Stop play') ||
+                        (report['Sensor Type'] == 'General' && report['Sensor Value'] == 'idle')) {
                         return false;
                     }
                     return null;
@@ -73,4 +73,22 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
             parser: value => new Buffer([value / 30])
         }
     }
+});
+
+Homey.manager('flow').on('action.PH-PSE02-1B.EU-turn_alarm_on', (callback, args) => {
+    const node = module.exports.nodes[args.device.token];
+    if (node && node.hasOwnProperty('instance') && node.instance.hasOwnProperty('CommandClass') && node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY']) {
+        node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY'].SWITCH_BINARY_SET({
+            'Switch Value': 255
+        }, (err, result) => callback(err, result));
+    } else return callback('invalid_device_command_class');
+});
+
+Homey.manager('flow').on('action.PH-PSE02-1B.EU-turn_alarm_off', (callback, args) => {
+    const node = module.exports.nodes[args.device.token];
+    if (node && node.hasOwnProperty('instance') && node.instance.hasOwnProperty('CommandClass') && node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY']) {
+        node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY'].SWITCH_BINARY_SET({
+            'Switch Value': 0
+        }, (err, result) => callback(err, result));
+    } else return callback('invalid_device_command_class');
 });

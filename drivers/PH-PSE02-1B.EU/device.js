bae583c300dc2632a0a8eb4bca794781884f1f4c
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2017-12-22 23:17:43 +0100

    fixes after testing devices with SDK 2

--- drivers/PH-PSE02-1B.EU/device.js
+++ drivers/PH-PSE02-1B.EU/device.js
@@ -3,6 +3,7 @@
 // @TODO: TEST!
 
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+const Homey = require('homey');
 
 class ZipatoDevice extends ZwaveDevice {
 
@@ -12,39 +13,38 @@ class ZipatoDevice extends ZwaveDevice {
 		this.printNode();
 
 		// register the onoff capability with COMMAND_CLASS_SWITCH_BINARY or COMMAND_CLASS_BASIC
-		// @TODO: TEST!
-		this.registerCapability('onoff', ['SWITCH_BINARY', 'BASIC']);
+		this.registerCapability('onoff', 'SWITCH_BINARY');
+		this.registerCapability('onoff', 'BASIC');
 
 		// register the alarm_tamper capability with COMMAND_CLASS_NOTIFICATION or COMMAND_CLASS_SENSOR_BINARY
-		// @TODO: TEST!
-		this.registerCapability('alarm_tamper', ['NOTIFICATION', 'SENSOR_BINARY']);
+		this.registerCapability('alarm_tamper', 'NOTIFICATION');
+		this.registerCapability('alarm_tamper', 'SENSOR_BINARY');
 
 		// register a settings parser
-		// @TODO: TEST!
 		this.registerSetting('config_param_31', value => {			
 			return new Buffer([value / 30])
 		});
+
+		let turnAlarmOnFlow = new Homey.FlowCardAction('PH-PSE02-1B.EU-turn_alarm_on');
+		turnAlarmOnFlow
+		    .register()
+		    .registerRunListener(( args, state ) => {
+
+		    	return args.device.getCommandClass("SWITCH_BINARY").SWITCH_BINARY_SET({
+				    'Switch Value': 255
+				});
+		    });
+
+		let turnAlarmOffFlow = new Homey.FlowCardAction('PH-PSE02-1B.EU-turn_alarm_off');
+		turnAlarmOffFlow
+		    .register()
+		    .registerRunListener(( args, state ) => {
+
+		    	return args.device.getCommandClass("SWITCH_BINARY").SWITCH_BINARY_SET({
+				    'Switch Value': 0
+				});
+		    });		
 	}
 }
-
-/*
-Homey.manager('flow').on('action.PH-PSE02-1B.EU-turn_alarm_on', (callback, args) => {
-    const node = module.exports.nodes[args.device.token];
-    if (node && node.hasOwnProperty('instance') && node.instance.hasOwnProperty('CommandClass') && node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY']) {
-        node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY'].SWITCH_BINARY_SET({
-            'Switch Value': 255
-        }, (err, result) => callback(err, result));
-    } else return callback('invalid_device_command_class');
-});
-
-Homey.manager('flow').on('action.PH-PSE02-1B.EU-turn_alarm_off', (callback, args) => {
-    const node = module.exports.nodes[args.device.token];
-    if (node && node.hasOwnProperty('instance') && node.instance.hasOwnProperty('CommandClass') && node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY']) {
-        node.instance.CommandClass['COMMAND_CLASS_SWITCH_BINARY'].SWITCH_BINARY_SET({
-            'Switch Value': 0
-        }, (err, result) => callback(err, result));
-    } else return callback('invalid_device_command_class');
-});
-*/
 
 module.exports = ZipatoDevice;

1e890f3d7e44df484e55e81425e2c0c68731b1c1
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2017-12-24 21:15:59 +0100

    added settings for MH-P220.EU device

--- drivers/MH-P220.EU/device.js
+++ drivers/MH-P220.EU/device.js
@@ -6,14 +6,56 @@ class ZipatoDevice extends ZwaveDevice {
 
 	async onMeshInit() {
 
-		this.enableDebug();
-		this.printNode();
+		//this.enableDebug();
+		//this.printNode();
 
 		// register the onoff capability with COMMAND_CLASS_SWITCH_MULTILEVEL
 		this.registerCapability('onoff', 'SWITCH_MULTILEVEL');
 
-		// register the onoff capability with COMMAND_CLASS_SWITCH_MULTILEVEL
+		// register the dim capability with COMMAND_CLASS_SWITCH_MULTILEVEL
 		this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+
+		// settings parser for "Single dimming step time in ms (manual)"
+		this.registerSetting('config_param_7', value => {			
+			const buf = Buffer.alloc(2);
+			buf.writeUIntBE(parseInt(value/10), 0, 2);
+			return buf;
+		});
+
+		// settings parser for "Single dimming step time in ms (auto)"
+		this.registerSetting('config_param_9', value => {			
+			const buf = Buffer.alloc(2);
+			buf.writeUIntBE(parseInt(value/10), 0, 2);
+			return buf;
+		});
+
+		// ----------------------------------------
+		// START QUICK FIX: custom settings parser because ZwaveDevice cannot handle unsigned values yet
+
+		this.registerSetting('config_param_4', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+		this.registerSetting('config_param_15', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+		this.registerSetting('config_param_17', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+		this.registerSetting('config_param_18', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+		this.registerSetting('config_param_19', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+		this.registerSetting('config_param_20', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+		this.registerSetting('config_param_21', value => {			
+			return Buffer.alloc(1,parseInt(value));
+		});
+
+		// END QUICK FIX
+		// ----------------------------------------
 	}
 }
 

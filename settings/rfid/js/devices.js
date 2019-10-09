8bdc2bd29d28a91ce4b5c75e89d93bdae96b683f
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2019-10-09 21:53:49 +0200

    rewrite settings

--- settings/rfid/js/devices.js
+++ settings/rfid/js/devices.js
@@ -1,133 +1,174 @@
-var tagStatus = null;
+(function(app) {
+	var tagStatus = null;
 
-function loadTags()
-{
-	Homey.get('tagContainer', function(err, tags)
+	/**
+	 * reload tags
+	 */
+	function loadTags()
 	{
-		if(err) {
-			showMessage('error getting tagContainer', err, 'danger');
-			return;
-		}
-
-		var systemTagsContent = document.getElementById('systemTags');
-
-		if(typeof tags === 'undefined' || tags === null || tags) {
-			systemTagsContent.innerText = __('settings.rfid.messages.noTags');
-			return;
-		}
-		
-		systemTagsContent.innerHTML = '';
-		for (var i=0; i<tags.length; i++)
+		app.homey.get('tagContainer', function(err, tags)
 		{
-			var tag = tags[i];
-			console.log(tag);
+			if(err) {
+				app.message.show('error getting tagContainer', err, 'danger');
+				return;
+			}
 
-			var rows = new Array();
+			var systemTagsContent = document.getElementById('systemTags');
 
-			//add id
-			rows.push({
-				label: __('settings.devices.table.deviceId'),
-				value: tag.tagId
-			});
-/*
-			//add last updated
-			var date = new Date(device.lastUpdate);
-			rows.push({
-				label: __('settings.devices.table.lastUpdated'),
-				value: date.toString()
-			});
-*/
-			systemTagsContent.appendChild(createTable(rows));
-		}
-	});
-}
+			if(typeof tags === 'undefined' || tags === null || tags.length === 0) {
+				systemTagsContent.innerText = __('settings.rfid.messages.noTags');
+				return;
+			}
+			
+			systemTagsContent.innerHTML = '';
+			for (var i=0; i<tags.length; i++)
+			{
+				var tag = tags[i];
 
-function loadDevices()
-{
-	Homey.get('tagReaders', function(err, devices)
+				var rows = new Array();
+
+				//add id
+				rows.push({
+					label: __('settings.devices.table.deviceId'),
+					value: tag.tagId
+				});
+	/*
+				//add last updated
+				var date = new Date(device.lastUpdate);
+				rows.push({
+					label: __('settings.devices.table.lastUpdated'),
+					value: date.toString()
+				});
+	*/
+				systemTagsContent.appendChild(app.createTable(rows));
+			}
+		});
+	}
+
+	/**
+	 * reload devices
+	 */
+	function loadDevices()
 	{
-		if(err) {
-			showMessage('error getting tagReaders', err, 'danger');
-			return;
-		}
-
-		var systemDevicesContent = document.getElementById('systemDevices');
-		
-		if(typeof devices === 'undefined' || devices === null || devices.length == 0) {
-			systemDevicesContent.innerText = __('settings.rfid.messages.noReaders');
-			return;
-		}
-
-		systemDevicesContent.innerHTML = '';
-		for (var i=0; i<devices.length; i++)
+		app.homey.get('tagReaders', function(err, devices)
 		{
-			var device = devices[i];
-			var rows = new Array();
+			if(err) {
+				showMessage('error getting tagReaders', err, 'danger');
+				return;
+			}
 
-			//add id
-			rows.push({
-				label: __('settings.devices.table.deviceId'),
-				value: device.id
-			});
+			var systemDevicesContent = document.getElementById('systemDevices');
+			
+			if(typeof devices === 'undefined' || devices === null || devices.length === 0) {
+				systemDevicesContent.innerText = __('settings.rfid.messages.noReaders');
+				return;
+			}
 
-			//add last updated
-			var date = new Date(device.lastUpdate);
-			rows.push({
-				label: __('settings.devices.table.lastUpdated'),
-				value: date.toString()
-			});
+			systemDevicesContent.innerHTML = '';
+			for (var i=0; i<devices.length; i++)
+			{
+				var device = devices[i];
+				var rows = new Array();
 
-			systemDevicesContent.appendChild(createTable(rows));
-		}
-	});
-}
+				//add id
+				rows.push({
+					label: __('settings.devices.table.deviceId'),
+					value: device.id
+				});
 
-function loadTagStatus()
-{
-	Homey.get('tagStatus', function(err, status)
+				//add last updated
+				var date = new Date(device.lastUpdate);
+				rows.push({
+					label: __('settings.devices.table.lastUpdated'),
+					value: date.toString()
+				});
+
+				systemDevicesContent.appendChild(app.createTable(rows));
+			}
+		});
+	}
+
+	/**
+	 * reload tag status
+	 */
+	function loadTagStatus()
 	{
-		if(err) {
-			showMessage('error getting tagStatus', err, 'danger');
-			return
+		app.homey.get('tagStatus', function(err, status)
+		{
+			if(err) {
+				app.message.show('error getting tagStatus', err, 'danger');
+				return
+			}
+
+			// save status
+			tagStatus = (status === true);
+			if(tagStatus === true) {
+				document.getElementById('currentTagStatus').innerHTML = __('settings.rfid.descriptions.allowTags');
+			} else {
+				document.getElementById('currentTagStatus').innerHTML = __('settings.rfid.descriptions.denyTags');
+			}
+		});
+	}
+
+	/**
+	 * clear tags
+	 */
+	function clearTags()
+	{
+		app.homey.confirm(__('settings.rfid.messages.confirmClearTags'), 'warning', function(err, result) {
+			if (result === true) {
+				app.homey.set('tagContainer', new Array());
+				app.message.show('', __('settings.rfid.messages.tagsClearedConfirmation'), 'success');
+			}
+		});
+	}
+
+	/**
+	 * toggle current tag status
+	 */
+	function toggleTagStatus()
+	{
+		app.homey.set('tagStatus', (tagStatus === false));
+	}
+
+	/**
+	 * handle settings save for this page
+	 */
+	function onSettingsSet(name)
+	{
+		if (name === 'tagContainer') {
+			loadTags();
 		}
 
-		// save status
-		tagStatus = (status === true);
-		if(tagStatus === true) {
-			document.getElementById('currentTagStatus').innerHTML = __('settings.rfid.descriptions.allowTags');
-		} else {
-			document.getElementById('currentTagStatus').innerHTML = __('settings.rfid.descriptions.denyTags');
+		if (name === 'tagStatus') {
+			app.message.show('', __('settings.rfid.messages.toggleTagStatusConfirmed'), 'success');
+			loadTagStatus();
 		}
-	});
-}
+	}
 
-/**
- * clear tags
- */
-function clearTags()
-{
-	Homey.confirm(__('settings.rfid.messages.confirmClearTags'), 'warning', function(err, result) {
-		if (result === true) {
-			Homey.set('tagContainer', new Array());
-			showMessage('', __('settings.rfid.messages.tagsClearedConfirmation'), 'success');
+	//bind events
+	document.getElementById('btnClearTags').onclick = function() {
+		clearTags();
+		return false;
+	}
+
+	//bind events
+	document.getElementById('btnToggleStatus').onclick = function() {
+		toggleTagStatus();
+		return false;
+	}	
+
+	//bind global events
+	app.on('settings.set', onSettingsSet);
+
+	//init
+	loadDevices();
+	loadTagStatus();
+	loadTags();
+
+	return {
+		destroy: function() {
+			app.off('settings.set', onSettingsSet);
 		}
-	});
-	
-	// Always return false to cancel the bubbling click events.
-	return false;
-}
-
-/**
- * toggle current tag status
- */
-function toggleTagStatus()
-{
-	Homey.set('tagStatus', (tagStatus === false), function() {
-		showMessage('', __('settings.rfid.messages.toggleTagStatusConfirmed'), 'success');
-		loadTagStatus();
-	});
-}
-
-loadDevices();
-loadTagStatus();
-loadTags();
+	}
+});
--- settings/rfid/js/logs.js
+++ settings/rfid/js/logs.js
@@ -1,89 +1,106 @@
-function loadEventLogs()
-{
-	Homey.get('systemEventLog', function(err, eventLogs) {
+(function(app) {
 
-		// handle error
-		if (err) {
-		    showMessage('error getting systemEventLog', err, 'danger');
-			return;
-		}
+	/**
+	 * reload event logs
+	 */
+	function loadEventLogs()
+	{
+		Homey.get('systemEventLog', function(err, eventLogs) {
 
-		var systemEventLogContent = document.getElementById('systemEventLogs');
-		
-		// check if eventLogs is loaded
-		if (typeof eventLogs === 'undefined' || eventLogs === null || eventLogs.length === 0) {
-			systemEventLogContent.innerText = __('settings.rfid.messages.noEventsYet');
-			return;
-		}
-
-		// clear log
-		systemEventLogContent.innerHTML = '';
-		
-		// load log
-		for (var i=0; i<eventLogs.length; i++) {
-			var eventLog = eventLogs[i];
-			var rows = new Array();
-
-			//add date
-			var date = new Date(eventLog.time);
-			rows.push({
-				label: __('settings.systemEventLog.table.datetime'),
-				value: date.toString()
-			});
-
-			//add event
-			rows.push({
-				label: __('settings.systemEventLog.table.event'),
-				value: __('settings.systemEventLog.eventTypes.s' + eventLog.statusCode)
-			});
-
-			//add device
-			if (eventLog.deviceId !== null || eventLog.deviceName !== null) {
-				rows.push({
-					label: __('settings.systemEventLog.table.device'),
-					value: (eventLog.deviceName !== null ? eventLog.deviceName : eventLog.deviceId)
-				});
+			// handle error
+			if (err) {
+			    showMessage('error getting systemEventLog', err, 'danger');
+				return;
 			}
 
-			//add tag
-			rows.push({
-				label: __('settings.systemEventLog.table.tagOrCode'),
-				value: eventLog.tagId
-			});
-
-			//add user
-			if (eventLog.userName !== null) {
-				rows.push({
-					label: __('settings.systemEventLog.table.person'),
-					value: eventLog.userName + ' (' + eventLog.userId + ')'
-				});
-			}
-
-			systemEventLogContent.appendChild(createTable(rows));
-		}
-	});
-}
-
-function clearEventLog()
-{
-	Homey.confirm(__('settings.rfid.messages.confirmClearEventLog'), 'warning', function(err, result) {
-		if (result === true) {
-
-			// reset logs
-			document.getElementById('systemEventLogs').innerText = __('settings.loading');
+			var systemEventLogContent = document.getElementById('systemEventLogs');
 			
-			// reset systemEventLog
-			Homey.set('systemEventLog', new Array());
-			showMessage('', __('settings.rfid.messages.eventLogClearedConfirmation'), 'success');
+			// check if eventLogs is loaded
+			if (typeof eventLogs === 'undefined' || eventLogs === null || eventLogs.length === 0) {
+				systemEventLogContent.innerText = __('settings.rfid.messages.noEventsYet');
+				return;
+			}
 
-			// Current page is nothing left todo, so close page and load previous page
-			previousPage();
-		}
-	});
+			// clear log
+			systemEventLogContent.innerHTML = '';
+			
+			// load log
+			for (var i=0; i<eventLogs.length; i++) {
+				var eventLog = eventLogs[i];
+				var rows = new Array();
 
-	// Always return false to cancel the bubbling click events.
-	return false;
-}
+				//add date
+				var date = new Date(eventLog.time);
+				rows.push({
+					label: __('settings.systemEventLog.table.datetime'),
+					value: date.toString()
+				});
 
-// init load event logs
-loadEventLogs();
+				//add event
+				rows.push({
+					label: __('settings.systemEventLog.table.event'),
+					value: __('settings.systemEventLog.eventTypes.s' + eventLog.statusCode)
+				});
+
+				//add device
+				if (eventLog.deviceId !== null || eventLog.deviceName !== null) {
+					rows.push({
+						label: __('settings.systemEventLog.table.device'),
+						value: (eventLog.deviceName !== null ? eventLog.deviceName : eventLog.deviceId)
+					});
+				}
+
+				//add tag
+				rows.push({
+					label: __('settings.systemEventLog.table.tagOrCode'),
+					value: eventLog.tagId
+				});
+
+				//add user
+				if (eventLog.userName !== null) {
+					rows.push({
+						label: __('settings.systemEventLog.table.person'),
+						value: eventLog.userName + ' (' + eventLog.userId + ')'
+					});
+				}
+
+				systemEventLogContent.appendChild(createTable(rows));
+			}
+		});
+	}
+
+	/**
+	 * clear current event log
+	 */
+	function clearEventLog()
+	{
+		Homey.confirm(__('settings.rfid.messages.confirmClearEventLog'), 'warning', function(err, result) {
+			if (result === true) {
+
+				// reset logs
+				document.getElementById('systemEventLogs').innerText = __('settings.loading');
+				
+				// reset systemEventLog
+				Homey.set('systemEventLog', new Array());
+				showMessage('', __('settings.rfid.messages.eventLogClearedConfirmation'), 'success');
+
+				// Current page is nothing left todo, so close page and load previous page
+				previousPage();
+			}
+		});
+
+		// Always return false to cancel the bubbling click events.
+		return false;
+	}
+
+	//bind events
+	document.getElementById('btnClearEventLog').onclick = function() {
+		clearEventLog();
+		return false;
+	}	
+
+	// init load event logs
+	loadEventLogs();
+
+	return {}
+});

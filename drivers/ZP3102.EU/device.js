5b01c8316c7f6e9370b1c70019c5de7dfe5cff32
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2019-11-23 20:28:43 +0100

    added manufacturerid for ZP3102 + added settings

--- drivers/ZP3102.EU/device.js
+++ drivers/ZP3102.EU/device.js
@@ -19,16 +19,22 @@ class ZipatoDevice extends ZwaveDevice {
 		// register the alarm_tamper capability with COMMAND_CLASS_NOTIFICATION
 		this.registerCapability('alarm_tamper', 'NOTIFICATION');
 
+		// register alternative commandclass when notification is not avaible
+		if (typeof this.node.CommandClass['COMMAND_CLASS_NOTIFICATION'] === 'undefined') {
+			this.registerCapability('alarm_motion', 'SENSOR_BINARY');
+			this.registerCapability('alarm_tamper', 'ALARM');
+		}
+
 		// register the measure_temperature capability with SENSOR_MULTILEVEL
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {getOpts: {getOnStart: false}});
 
 		// set motion status
-    let setMotionFlow = new Homey.FlowCardAction('ZP3102.EU-set_motion');
-    setMotionFlow
-        .register()
-        .registerRunListener(( args, state ) => {
-          return args.device.setCapabilityValue('alarm_motion', (args.motion == "1"))
-        });
+    	let setMotionFlow = new Homey.FlowCardAction('ZP3102.EU-set_motion');
+    	setMotionFlow
+        	.register()
+        	.registerRunListener(( args, state ) => {
+				return args.device.setCapabilityValue('alarm_motion', (args.motion == "1"))
+        	});
 	}
 }
 

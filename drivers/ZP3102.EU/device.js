902e3555389ffdc0c847cd0eddfd87d33a4edd35
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2020-02-01 17:06:59 +0100

    enable debugging for device VS-ZD2102

--- drivers/ZP3102.EU/device.js
+++ drivers/ZP3102.EU/device.js
@@ -7,8 +7,8 @@ class ZipatoDevice extends ZwaveDevice {
 
 	async onMeshInit() {
 
-		//this.enableDebug();
-		//this.printNode();
+		this.enableDebug();
+		this.printNode();
 
 		// register the measure_battery capability with COMMAND_CLASS_BATTERY
 		this.registerCapability('measure_battery', 'BATTERY');

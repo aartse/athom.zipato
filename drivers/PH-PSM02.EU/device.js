17d2960096e60228d44bf441029d576de7a7925d
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2021-01-16 16:36:10 +0100

    PST02 support + fixes for PSM02 settings

--- drivers/PH-PSM02.EU/device.js
+++ drivers/PH-PSM02.EU/device.js
@@ -4,6 +4,11 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 
 //https://www.zipato.com/wp-content/uploads/2015/09/ph-psm02-Zipato-Multisensor-Quad-User-Manual-v1.4.pdf
 
+//https://products.z-wavealliance.org/products/2433?selectedFrequencyId=-1 (ph-psm02.eu)
+//https://products.z-wavealliance.org/products/1449?selectedFrequencyId=-1 (PST02-1A)
+//https://products.z-wavealliance.org/products/1090?selectedFrequencyId=-1 (PST02-1B)
+//https://products.z-wavealliance.org/products/1092?selectedFrequencyId=-1 (PST02-1C)
+
 class ZipatoDevice extends ZwaveDevice {
 
 	async onNodeInit() {
@@ -11,13 +16,8 @@ class ZipatoDevice extends ZwaveDevice {
 		this.enableDebug();
 		this.printNode();
 
-		this.registerCapability('alarm_motion', 'SENSOR_BINARY');
 		this.registerCapability('alarm_motion', 'NOTIFICATION');
-
-		this.registerCapability('alarm_contact', 'SENSOR_BINARY');
 		this.registerCapability('alarm_contact', 'NOTIFICATION');
-
-		this.registerCapability('alarm_tamper', 'SENSOR_BINARY');
 		this.registerCapability('alarm_tamper', 'NOTIFICATION');
 
 		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');

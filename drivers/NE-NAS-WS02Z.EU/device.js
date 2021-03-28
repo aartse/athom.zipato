8a97783258d6dc8bfdf38957c20744df2698e7ee
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2021-03-28 15:15:58 +0200

    finetuning

--- drivers/NE-NAS-WS02Z.EU/device.js
+++ drivers/NE-NAS-WS02Z.EU/device.js
@@ -5,7 +5,7 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 module.exports = class ZipatoDevice extends ZwaveDevice {
 
   async onNodeInit() {
-    this.registerCapability('alarm_water', 'SENSOR_BINARY');
+    this.registerCapability('alarm_water', 'NOTIFICATION');
     this.registerCapability('measure_battery', 'BATTERY');
   }
 
